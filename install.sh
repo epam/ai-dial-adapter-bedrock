86c833e9028ef2ce06c03b6744c1cb8b3451e88a
Anton Dubovik <anton_dubovik@epam.com>
2023-06-21 15:00:35 +0100

    Frozen requirements

--- install.sh
+++ install.sh
@@ -2,6 +2,3 @@ python3 -m venv .venv
 source .venv/bin/activate
 
 pip install -r requirements.txt
-pip install ./bedrock/botocore-1.29.142-py3-none-any.whl
-pip install ./bedrock/boto3-1.26.142-py3-none-any.whl
-pip install ./bedrock/awscli-1.27.142-py3-none-any.whl
