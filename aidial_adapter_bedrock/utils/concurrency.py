f8142d6bfb8f36d7f9f379fa44e5af593071b59b
Anton Dubovik <anton_dubovik@epam.com>
2023-12-05 12:40:32 +0000

    fix: review fixes

--- aidial_adapter_bedrock/utils/concurrency.py
+++ aidial_adapter_bedrock/utils/concurrency.py
@@ -1,12 +1,20 @@
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
-from typing import AsyncIterator, Callable, Iterator, Optional, Tuple, TypeVar
+from typing import (
+    AsyncIterator,
+    Callable,
+    Iterator,
+    Optional,
+    Tuple,
+    TypeVar,
+    cast,
+)
 
 T = TypeVar("T")
 
 
 async def make_async(func: Callable[[], T]) -> T:
-    with ThreadPoolExecutor() as executor:
+    with ThreadPoolExecutor(max_workers=1) as executor:
         loop = asyncio.get_event_loop()
         return await loop.run_in_executor(executor, func)
 
@@ -23,5 +31,4 @@ async def to_async_iterator(iter: Iterator[T]) -> AsyncIterator[T]:
         if is_end:
             break
         else:
-            assert item is not None
-            yield item
+            yield cast(T, item)
