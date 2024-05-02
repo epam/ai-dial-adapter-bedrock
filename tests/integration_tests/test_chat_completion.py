0065ef8a7742e71905702f6c6a9ac9936634d776
Anton Dubovik <anton_dubovik@epam.com>
2024-05-02 17:46:25 +0200

    feat: add tests for tool and function usage (#100)

--- tests/integration_tests/test_chat_completion.py
+++ tests/integration_tests/test_chat_completion.py
@@ -1,28 +1,114 @@
+import json
 import re
 from dataclasses import dataclass
-from typing import Callable, List, Optional
+from typing import Any, Callable, List
 
 import pytest
-from langchain_core.messages import BaseMessage
-from openai import APIStatusError, BadRequestError, UnprocessableEntityError
+from openai import APIError, BadRequestError, UnprocessableEntityError
+from openai.types.chat import (
+    ChatCompletionMessageParam,
+    ChatCompletionMessageToolCall,
+    ChatCompletionToolParam,
+)
+from openai.types.chat.chat_completion_message import FunctionCall
+from openai.types.chat.completion_create_params import Function
 from pydantic import BaseModel
 
 from aidial_adapter_bedrock.llm.bedrock_models import BedrockDeployment
 from tests.conftest import TEST_SERVER_URL
-from tests.utils.langchain import (
+from tests.utils.openai import (
+    ChatCompletionResult,
     ai,
-    create_chat_model,
-    run_model,
+    ai_function,
+    ai_tools,
+    chat_completion,
+    function_request,
+    function_response,
+    function_to_tool,
+    get_client,
     sanitize_test_name,
     sys,
+    tool_request,
+    tool_response,
     user,
 )
 
+get_weather_function: Function = {
+    "name": "get_current_weather",
+    "description": "Get the current weather",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "location": {
+                "type": "string",
+                "description": "The city and state, e.g. San Francisco, CA",
+            },
+            "format": {
+                "type": "string",
+                "enum": ["celsius", "fahrenheit"],
+                "description": "The temperature unit to use. Infer this from the users location.",
+            },
+        },
+        "required": ["location", "format"],
+    },
+}
+
+
+def match_objects(expected: Any, actual: Any) -> None:
+    if isinstance(expected, dict):
+        assert list(sorted(expected.keys())) == list(sorted(actual.keys()))
+        for k, v in expected.items():
+            match_objects(v, actual[k])
+    elif isinstance(expected, tuple):
+        assert len(expected) == len(actual)
+        for i in range(len(expected)):
+            match_objects(expected[i], actual[i])
+    elif isinstance(expected, list):
+        assert len(expected) == len(actual)
+        for i in range(len(expected)):
+            match_objects(expected[i], actual[i])
+    elif callable(expected):
+        assert expected(actual)
+    else:
+        assert expected == actual
+
+
+def is_valid_function_call(
+    call: FunctionCall | None, function: Function, expected_args: Any
+) -> bool:
+    assert call is not None
+    assert call.name == function["name"]
+    obj = json.loads(call.arguments)
+    match_objects(expected_args, obj)
+    return True
+
+
+def is_valid_tool_calls(
+    calls: List[ChatCompletionMessageToolCall] | None,
+    function: Function,
+    expected_args: Any,
+) -> bool:
+    assert calls is not None
+    assert len(calls) == 1
+    call = calls[0]
+
+    function_call = call.function
+    assert call.id == function["name"]
+    assert function_call.name == function["name"]
+
+    obj = json.loads(function_call.arguments)
+    match_objects(expected_args, obj)
+    return True
+
 
 class ExpectedException(BaseModel):
-    type: type[APIStatusError]
+    type: type[APIError]
     message: str
-    status_code: int
+    status_code: int | None = None
+
+
+def expected_success(*args, **kwargs):
+    return True
 
 
 @dataclass
@@ -32,17 +118,23 @@ class TestCase:
     name: str
     deployment: BedrockDeployment
     streaming: bool
-    max_tokens: Optional[int]
-    stop: Optional[List[str]]
 
-    messages: List[BaseMessage]
-    test: Callable[[str], bool] | ExpectedException
+    messages: List[ChatCompletionMessageParam]
+
+    expected: Callable[[ChatCompletionResult], bool] | ExpectedException
+
+    max_tokens: int | None
+    stop: List[str] | None
+
+    functions: List[Function] | None
+    tools: List[ChatCompletionToolParam] | None
 
     def get_id(self):
-        max_tokens_str = str(self.max_tokens) if self.max_tokens else "inf"
-        stop_sequence_str = str(self.stop) if self.stop else "nonstop"
+        max_tokens_str = f"maxt={self.max_tokens}" if self.max_tokens else ""
+        stop_sequence_str = f"stop={self.stop}" if self.stop else ""
         return sanitize_test_name(
-            f"{self.deployment.value} {self.streaming} {max_tokens_str} {stop_sequence_str} {self.name}"
+            f"{self.deployment.value} {self.streaming} {max_tokens_str} "
+            f"{stop_sequence_str} {self.name}"
         )
 
 
@@ -52,87 +144,103 @@ chat_deployments = [
     BedrockDeployment.AI21_J2_JUMBO_INSTRUCT,
     BedrockDeployment.ANTHROPIC_CLAUDE_INSTANT_V1,
     BedrockDeployment.ANTHROPIC_CLAUDE_V2,
+    BedrockDeployment.ANTHROPIC_CLAUDE_V2_1,
     BedrockDeployment.ANTHROPIC_CLAUDE_V3_SONNET,
     BedrockDeployment.META_LLAMA2_70B_CHAT_V1,
     BedrockDeployment.COHERE_COMMAND_TEXT_V14,
 ]
 
 
+def supports_tools(deployment: BedrockDeployment) -> bool:
+    return deployment in [
+        BedrockDeployment.ANTHROPIC_CLAUDE_V2_1,
+        BedrockDeployment.ANTHROPIC_CLAUDE_V3_SONNET,
+        BedrockDeployment.ANTHROPIC_CLAUDE_V3_HAIKU,
+        BedrockDeployment.ANTHROPIC_CLAUDE_V3_OPUS,
+    ]
+
+
 def get_test_cases(
     deployment: BedrockDeployment, streaming: bool
 ) -> List[TestCase]:
-    ret: List[TestCase] = []
+    test_cases: List[TestCase] = []
 
-    ret.append(
-        TestCase(
-            name="dialog recall",
-            deployment=deployment,
-            streaming=streaming,
-            max_tokens=None,
-            stop=None,
-            messages=[
-                user("my name is Anton"),
-                ai("nice to meet you"),
-                user("what's my name?"),
-            ],
-            test=lambda s: "anton" in s.lower(),
+    def streaming_error(exc: ExpectedException) -> ExpectedException:
+        if streaming:
+            return ExpectedException(
+                type=APIError,
+                message="An error occurred during streaming",
+            )
+        else:
+            return exc
+
+    def test_case(
+        name: str,
+        messages: List[ChatCompletionMessageParam],
+        expected: (
+            Callable[[ChatCompletionResult], bool] | ExpectedException
+        ) = expected_success,
+        max_tokens: int | None = None,
+        stop: List[str] | None = None,
+        functions: List[Function] | None = None,
+        tools: List[ChatCompletionToolParam] | None = None,
+    ) -> None:
+        test_cases.append(
+            TestCase(
+                name,
+                deployment,
+                streaming,
+                messages,
+                expected,
+                max_tokens,
+                stop,
+                functions,
+                tools,
+            )
         )
+
+    test_case(
+        name="dialog recall",
+        messages=[
+            user("my name is Anton"),
+            ai("nice to meet you"),
+            user("what's my name?"),
+        ],
+        expected=lambda s: "anton" in s.content.lower(),
     )
 
-    ret.append(
-        TestCase(
-            name="2+3=5",
-            deployment=deployment,
-            streaming=streaming,
-            max_tokens=None,
-            stop=None,
-            messages=[user("compute 2+3")],
-            test=lambda s: "5" in s,
-        )
+    test_case(
+        name="2+3=5",
+        messages=[user("compute (2+3)")],
+        expected=lambda s: "5" in s.content,
     )
 
-    ret.append(
-        TestCase(
-            name="empty system message",
-            deployment=deployment,
-            streaming=streaming,
-            max_tokens=None,
-            stop=None,
-            messages=[sys(""), user("compute 2+4")],
-            test=lambda s: "6" in s,
-        )
+    test_case(
+        name="empty system message",
+        messages=[sys(""), user("compute (2+4)")],
+        expected=lambda s: "6" in s.content,
     )
 
     query = 'Reply with "Hello"'
     if deployment == BedrockDeployment.ANTHROPIC_CLAUDE_INSTANT_V1:
         query = 'Print "Hello"'
 
-    ret.append(
-        TestCase(
-            name="hello",
-            deployment=deployment,
-            streaming=streaming,
-            max_tokens=None,
-            stop=None,
-            messages=[user(query)],
-            test=lambda s: "hello" in s.lower() or "hi" in s.lower(),
-        )
+    test_case(
+        name="hello",
+        messages=[user(query)],
+        expected=lambda s: "hello" in s.content.lower()
+        or "hi" in s.content.lower(),
     )
 
-    ret.append(
-        TestCase(
-            name="empty dialog",
-            deployment=deployment,
-            streaming=streaming,
-            max_tokens=1,
-            stop=None,
-            messages=[],
-            test=ExpectedException(
-                type=UnprocessableEntityError,
-                message="List of messages must not be empty",
-                status_code=422,
-            ),
-        )
+    test_case(
+        name="empty dialog",
+        max_tokens=1,
+        messages=[],
+        expected=ExpectedException(
+            type=UnprocessableEntityError,
+            message="List of messages must not be empty",
+            status_code=422,
+        ),
     )
 
     is_claude3 = deployment in [
@@ -141,76 +249,113 @@ def get_test_cases(
         BedrockDeployment.ANTHROPIC_CLAUDE_V3_OPUS,
     ]
 
-    ret.append(
-        TestCase(
-            name="empty user message",
-            deployment=deployment,
-            streaming=streaming,
-            max_tokens=1,
-            stop=None,
-            messages=[user("")],
-            test=(
+    test_case(
+        name="empty user message",
+        max_tokens=1,
+        messages=[user("")],
+        expected=(
+            streaming_error(
                 ExpectedException(
                     type=BadRequestError,
-                    message="all messages must have non-empty content except for the optional final assistant message",
+                    message="messages: text content blocks must be non-empty",
                     status_code=400,
                 )
-                if is_claude3
-                else lambda s: True
-            ),
-        )
+            )
+            if is_claude3
+            else expected_success
+        ),
     )
 
-    ret.append(
-        TestCase(
-            name="single space user message",
-            deployment=deployment,
-            streaming=streaming,
-            max_tokens=1,
-            stop=None,
-            messages=[user(" ")],
-            test=(
+    test_case(
+        name="single space user message",
+        max_tokens=1,
+        messages=[user(" ")],
+        expected=(
+            streaming_error(
                 ExpectedException(
                     type=BadRequestError,
-                    message="text content blocks must contain non-whitespace text",
+                    message="messages: text content blocks must contain non-whitespace text",
                     status_code=400,
                 )
-                if is_claude3
-                else lambda s: True
-            ),
-        )
+            )
+            if is_claude3
+            else expected_success
+        ),
     )
 
-    ret.append(
-        TestCase(
-            name="max tokens 1",
-            deployment=deployment,
-            streaming=streaming,
-            max_tokens=1,
-            stop=None,
-            messages=[user("tell me the full story of Pinocchio")],
-            test=lambda s: len(s.split()) <= 1,
-        )
+    test_case(
+        name="max tokens 1",
+        max_tokens=1,
+        messages=[user("tell me the full story of Pinocchio")],
+        expected=lambda s: len(s.content.split()) <= 1,
     )
 
     # ai21 models do not support more than one stop word
-    stop = ["world", "World"]
     if "ai21" in deployment.model_id:
-        stop = ["world"]
+        stop = ["John"]
+    else:
+        stop = ["John", "john"]
 
-    ret.append(
-        TestCase(
-            name="stop sequence",
-            deployment=deployment,
-            streaming=streaming,
-            max_tokens=None,
-            stop=stop,
-            messages=[user('Reply with "hello world"')],
-            test=lambda s: "world" not in s.lower(),
-        )
+    test_case(
+        name="stop sequence",
+        stop=stop,
+        messages=[user('Reply with "John"')],
+        expected=lambda s: "John" not in s.content.lower(),
     )
 
-    return ret
+    if supports_tools(deployment):
+        query = "What's the temperature in Glasgow in celsius?"
+
+        function_args_checker = {
+            "location": lambda s: "glasgow" in s.lower(),
+            "format": "celsius",
+        }
+
+        function_args = {"location": "Glasgow", "format": "celsius"}
+
+        name = get_weather_function["name"]
+
+        # Functions
+        test_case(
+            name="weather function",
+            messages=[user(query)],
+            functions=[get_weather_function],
+            expected=lambda s: is_valid_function_call(
+                s.function_call, get_weather_function, function_args_checker
+            ),
+        )
+
+        function_req = ai_function(function_request(name, function_args))
+        function_resp = function_response(name, "15 celsius")
+
+        test_case(
+            name="weather function followup",
+            messages=[user(query), function_req, function_resp],
+            functions=[get_weather_function],
+            expected=lambda s: "15" in s.content.lower(),
+        )
+
+        # Tools
+        test_case(
+            name="weather tool",
+            messages=[user(query)],
+            tools=[function_to_tool(get_weather_function)],
+            expected=lambda s: is_valid_tool_calls(
+                s.tool_calls, get_weather_function, function_args_checker
+            ),
+        )
+
+        tool_req = ai_tools([tool_request(name, function_args)])
+        tool_resp = tool_response(name, "15 celsius")
+
+        test_case(
+            name="weather tool followup",
+            messages=[user(query), tool_req, tool_resp],
+            tools=[function_to_tool(get_weather_function)],
+            expected=lambda s: "15" in s.content.lower(),
+        )
+
+    return test_cases
 
 
 @pytest.mark.asyncio
@@ -224,22 +369,32 @@ def get_test_cases(
     ],
     ids=lambda test: test.get_id(),
 )
-async def test_chat_completion_langchain(server, test: TestCase):
-    model = create_chat_model(
-        TEST_SERVER_URL, test.deployment.value, test.streaming, test.max_tokens
-    )
+async def test_chat_completion_openai(server, test: TestCase):
+    client = get_client(TEST_SERVER_URL, test.deployment.value)
 
-    if isinstance(test.test, ExpectedException):
-        with pytest.raises(test.test.type) as exc_info:
-            await run_model(model, test.messages, test.streaming, test.stop)
-
-        assert isinstance(exc_info.value, test.test.type)
-        assert exc_info.value.status_code == test.test.status_code
-        assert re.search(test.test.message, str(exc_info.value))
-    else:
-        actual_output = await run_model(
-            model, test.messages, test.streaming, test.stop
+    async def run_chat_completion() -> ChatCompletionResult:
+        return await chat_completion(
+            client,
+            test.messages,
+            test.streaming,
+            test.stop,
+            test.max_tokens,
+            test.functions,
+            test.tools,
         )
-        assert test.test(
+
+    if isinstance(test.expected, ExpectedException):
+        with pytest.raises(Exception) as exc_info:
+            await run_chat_completion()
+
+        actual_exc = exc_info.value
+
+        assert isinstance(actual_exc, test.expected.type)
+        actual_status_code = getattr(actual_exc, "status_code", None)
+        assert actual_status_code == test.expected.status_code
+        assert re.search(test.expected.message, str(actual_exc))
+    else:
+        actual_output = await run_chat_completion()
+        assert test.expected(
             actual_output
         ), f"Failed output test, actual output: {actual_output}"
---
+++ tests/utils/openai.py
@@ -0,0 +1,163 @@
+import json
+import re
+from typing import Any, AsyncGenerator, List, Optional
+
+from aidial_sdk.utils.streaming import merge_chunks
+from openai import AsyncAzureOpenAI, AsyncStream
+from openai._types import NOT_GIVEN
+from openai.types import CompletionUsage
+from openai.types.chat import (
+    ChatCompletion,
+    ChatCompletionAssistantMessageParam,
+    ChatCompletionFunctionMessageParam,
+    ChatCompletionMessageParam,
+    ChatCompletionMessageToolCall,
+    ChatCompletionMessageToolCallParam,
+    ChatCompletionSystemMessageParam,
+    ChatCompletionToolMessageParam,
+    ChatCompletionToolParam,
+    ChatCompletionUserMessageParam,
+)
+from openai.types.chat.chat_completion_message import (
+    ChatCompletionMessage,
+    FunctionCall,
+)
+from openai.types.chat.chat_completion_message_tool_call_param import (
+    Function as ToolFunction,
+)
+from openai.types.chat.completion_create_params import Function
+from openai.types.shared_params.function_definition import FunctionDefinition
+from pydantic import BaseModel
+
+from tests.conftest import DEFAULT_API_VERSION
+
+
+def sys(content: str) -> ChatCompletionSystemMessageParam:
+    return {"role": "system", "content": content}
+
+
+def ai(content: str) -> ChatCompletionAssistantMessageParam:
+    return {"role": "assistant", "content": content}
+
+
+def ai_function(
+    function_call: ToolFunction,
+) -> ChatCompletionAssistantMessageParam:
+    return {"role": "assistant", "function_call": function_call}
+
+
+def ai_tools(
+    tool_calls: List[ChatCompletionMessageToolCallParam],
+) -> ChatCompletionAssistantMessageParam:
+    return {"role": "assistant", "tool_calls": tool_calls}
+
+
+def user(content: str) -> ChatCompletionUserMessageParam:
+    return {"role": "user", "content": content}
+
+
+def function_request(name: str, args: Any) -> ToolFunction:
+    return {"name": name, "arguments": json.dumps(args)}
+
+
+def tool_request(name: str, args: Any) -> ChatCompletionMessageToolCallParam:
+    return {
+        "id": name,
+        "type": "function",
+        "function": function_request(name, args),
+    }
+
+
+def function_response(
+    name: str, content: str
+) -> ChatCompletionFunctionMessageParam:
+    return {"role": "function", "name": name, "content": content}
+
+
+def tool_response(name: str, content: str) -> ChatCompletionToolMessageParam:
+    return {"role": "tool", "tool_call_id": name, "content": content}
+
+
+def function_to_tool(function: FunctionDefinition) -> ChatCompletionToolParam:
+    return {"type": "function", "function": function}
+
+
+def sanitize_test_name(name: str) -> str:
+    name2 = "".join(c if c.isalnum() else "_" for c in name.lower())
+    return re.sub("_+", "_", name2)
+
+
+class ChatCompletionResult(BaseModel):
+    response: ChatCompletion
+
+    @property
+    def message(self) -> ChatCompletionMessage:
+        return self.response.choices[0].message
+
+    @property
+    def content(self) -> str:
+        return self.message.content or ""
+
+    @property
+    def usage(self) -> CompletionUsage | None:
+        return self.response.usage
+
+    @property
+    def function_call(self) -> FunctionCall | None:
+        return self.message.function_call
+
+    @property
+    def tool_calls(self) -> List[ChatCompletionMessageToolCall] | None:
+        return self.message.tool_calls
+
+
+async def chat_completion(
+    client: AsyncAzureOpenAI,
+    messages: List[ChatCompletionMessageParam],
+    stream: bool,
+    stop: Optional[List[str]],
+    max_tokens: Optional[int],
+    functions: List[Function] | None,
+    tools: List[ChatCompletionToolParam] | None,
+) -> ChatCompletionResult:
+    async def get_response() -> ChatCompletion:
+        response = await client.chat.completions.create(
+            model="dummy_model",
+            messages=messages,
+            stream=stream,
+            stop=stop,
+            max_tokens=max_tokens,
+            temperature=0.0,
+            function_call="auto" if functions is not None else NOT_GIVEN,
+            functions=functions or NOT_GIVEN,
+            tool_choice="auto" if tools is not None else NOT_GIVEN,
+            tools=tools or NOT_GIVEN,
+        )
+
+        if isinstance(response, AsyncStream):
+
+            async def generator() -> AsyncGenerator[dict, None]:
+                async for chunk in response:
+                    yield chunk.dict()
+
+            response_dict = await merge_chunks(generator())
+            response_dict["object"] = "chat.completion"
+            response_dict["model"] = "dummy_model"
+
+            return ChatCompletion.parse_obj(response_dict)
+        else:
+            return response
+
+    response = await get_response()
+    return ChatCompletionResult(response=response)
+
+
+def get_client(base_url: str, model_id: str) -> AsyncAzureOpenAI:
+    return AsyncAzureOpenAI(
+        azure_endpoint=base_url,
+        azure_deployment=model_id,
+        api_version=DEFAULT_API_VERSION,
+        api_key="dummy_key",
+        max_retries=0,
+        timeout=10,
+    )
