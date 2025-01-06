3a0aa5765e9889f4f469aa2b70b4c8fd92d6b3b5
Anton Dubovik <anton_dubovik@epam.com>
2025-01-06 14:39:41 +0000

    chore: removed repetition in the deployment match case

--- aidial_adapter_bedrock/llm/model/adapter.py
+++ aidial_adapter_bedrock/llm/model/adapter.py
@@ -153,9 +153,7 @@ async def get_bedrock_adapter(
                 supported_document_types=[],
             )
         case (
-            ChatCompletionDeployment.META_LLAMA3_8B_INSTRUCT_V1
-            | ChatCompletionDeployment.META_LLAMA3_70B_INSTRUCT_V1
-            | ChatCompletionDeployment.META_LLAMA3_1_8B_INSTRUCT_V1
+            ChatCompletionDeployment.META_LLAMA3_1_8B_INSTRUCT_V1
             | ChatCompletionDeployment.META_LLAMA3_2_1B_INSTRUCT_V1
             | ChatCompletionDeployment.META_LLAMA3_2_3B_INSTRUCT_V1
         ):
