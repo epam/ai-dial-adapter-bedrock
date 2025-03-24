7e2e1d8c4e4f1ee356fc78e02a4a238336282d81
Anton Dubovik <anton_dubovik@epam.com>
2025-03-24 14:07:54 +0100

    feat: add configuration and support `seed` parameter for Stability V2, V3 models (#224)

--- aidial_adapter_bedrock/llm/model/adapter.py
+++ aidial_adapter_bedrock/llm/model/adapter.py
@@ -96,24 +96,14 @@ async def get_bedrock_adapter(
         case (
             ChatCompletionDeployment.STABILITY_STABLE_IMAGE_CORE_V1
             | ChatCompletionDeployment.STABILITY_STABLE_IMAGE_ULTRA_V1
+            | ChatCompletionDeployment.STABILITY_STABLE_DIFFUSION_3_LARGE_V1
         ):
-            model = StabilityV2Adapter.create(
+            adapter = StabilityV2Adapter.create(
                 await Bedrock.acreate(aws_client_config),
-                model,
+                deployment.clone(deployment.reference_deployment_id),
                 api_key,
-                image_to_image_supported=False,
             )
-            return replicator_decorator()(model)
-        case ChatCompletionDeployment.STABILITY_STABLE_DIFFUSION_3_LARGE_V1:
-            model = StabilityV2Adapter.create(
-                await Bedrock.acreate(aws_client_config),
-                model,
-                api_key,
-                image_to_image_supported=True,
-                image_width_constraints=(640, 1536),
-                image_height_constraints=(640, 1536),
-            )
-            return replicator_decorator()(model)
+            return replicator_decorator()(adapter)
         case ChatCompletionDeployment.AMAZON_TITAN_TG1_LARGE:
             return amazon.create_adapter(
                 await Bedrock.acreate(aws_client_config), model
--- aidial_adapter_bedrock/llm/model/stability/v2.py
+++ aidial_adapter_bedrock/llm/model/stability/v2.py
@@ -1,12 +1,15 @@
 from io import BytesIO
-from typing import List, Optional, Tuple
+from typing import List, Literal, Optional, Tuple, Type
 
 from aidial_sdk.chat_completion import Attachment, Message
 from aidial_sdk.exceptions import RequestValidationError
 from PIL import Image
 from pydantic import BaseModel
+from typing_extensions import assert_never
 
+from aidial_adapter_bedrock.adapter_deployments import AdapterDeployment
 from aidial_adapter_bedrock.bedrock import Bedrock
+from aidial_adapter_bedrock.deployments import ChatCompletionDeployment
 from aidial_adapter_bedrock.dial_api.request import ModelParameters
 from aidial_adapter_bedrock.dial_api.resource import (
     DialResource,
@@ -43,36 +46,6 @@ async def _download_resource(
             error_message=f"Unsupported image type: {e.type}",
             usage_message=f"Supported image types: {', '.join(SUPPORTED_IMAGE_EXTENSIONS)}",
         )
-
-
-def _validate_image_size(
-    image: Resource,
-    width_constraints: Tuple[int, int] | None,
-    height_constraints: Tuple[int, int] | None,
-) -> None:
-    if width_constraints is None and height_constraints is None:
-        return
-
-    with Image.open(BytesIO(image.data)) as img:
-        width, height = img.size
-
-        for constraints, value, name in [
-            (width_constraints, width, "width"),
-            (height_constraints, height, "height"),
-        ]:
-            if constraints is None:
-                continue
-            min_value, max_value = constraints
-            if not (min_value <= value <= max_value):
-                error_msg = (
-                    f"Image {name} is {value}, but should be "
-                    f"between {min_value} and {max_value}"
-                )
-                raise RequestValidationError(
-                    message=error_msg,
-                    display_message=error_msg,
-                    code="invalid_argument",
-                )
 
 
 class StabilityV2Response(BaseModel):
@@ -114,33 +87,111 @@ class StabilityV2Response(BaseModel):
             raise ValidationError(error)
 
 
-class StabilityV2Adapter(ChatCompletionAdapter):
-    model: str
-    client: Bedrock
-    storage: Optional[FileStorage]
+AspectRatios = Literal[
+    "16:9", "1:1", "21:9", "2:3", "3:2", "4:5", "5:4", "9:16", "9:21"
+]
+
+
+class StabilityImageConfiguration(BaseModel):
+    class Config:
+        extra = "allow"
+
+    aspect_ratio: AspectRatios | str | None = None
+    negative_prompt: str | None = None
+
+
+class StabilityV3Configuration(StabilityImageConfiguration):
+    class Config:
+        extra = "allow"
+
+    cfg_scale: float | None = None
+
+
+Stability_V2_V3 = Literal[
+    ChatCompletionDeployment.STABILITY_STABLE_IMAGE_CORE_V1,
+    ChatCompletionDeployment.STABILITY_STABLE_IMAGE_ULTRA_V1,
+    ChatCompletionDeployment.STABILITY_STABLE_DIFFUSION_3_LARGE_V1,
+]
+
+
+class Spec(BaseModel):
     image_to_image_supported: bool
     width_constraints: Tuple[int, int] | None
     height_constraints: Tuple[int, int] | None
+    configuration_cls: Type[BaseModel]
+
+    def validate_image(self, image: Resource) -> None:
+        if self.width_constraints is None and self.height_constraints is None:
+            return
+
+        with Image.open(BytesIO(image.data)) as img:
+            width, height = img.size
+
+            for constraints, value, name in [
+                (self.width_constraints, width, "width"),
+                (self.height_constraints, height, "height"),
+            ]:
+                if constraints is None:
+                    continue
+                min_value, max_value = constraints
+                if not (min_value <= value <= max_value):
+                    error_msg = (
+                        f"Image {name} is {value}, but should be "
+                        f"between {min_value} and {max_value}"
+                    )
+                    raise RequestValidationError(
+                        message=error_msg,
+                        display_message=error_msg,
+                        code="invalid_argument",
+                    )
+
+
+def _get_spec(deployment: Stability_V2_V3) -> Spec:
+    match deployment:
+        case (
+            ChatCompletionDeployment.STABILITY_STABLE_IMAGE_CORE_V1
+            | ChatCompletionDeployment.STABILITY_STABLE_IMAGE_ULTRA_V1
+        ):
+            return Spec(
+                image_to_image_supported=False,
+                width_constraints=None,
+                height_constraints=None,
+                configuration_cls=StabilityImageConfiguration,
+            )
+        case ChatCompletionDeployment.STABILITY_STABLE_DIFFUSION_3_LARGE_V1:
+            return Spec(
+                image_to_image_supported=True,
+                width_constraints=(640, 1536),
+                height_constraints=(640, 1536),
+                configuration_cls=StabilityV3Configuration,
+            )
+        case _:
+            return assert_never(deployment)
+
+
+class StabilityV2Adapter(ChatCompletionAdapter):
+    deployment: AdapterDeployment[Stability_V2_V3]
+    client: Bedrock
+    storage: Optional[FileStorage]
+    spec: Spec
 
     @classmethod
     def create(
         cls,
         client: Bedrock,
-        model: str,
+        deployment: AdapterDeployment[Stability_V2_V3],
         api_key: str,
-        image_to_image_supported: bool,
-        image_width_constraints: Tuple[int, int] | None = None,
-        image_height_constraints: Tuple[int, int] | None = None,
     ):
         storage: Optional[FileStorage] = create_file_storage(api_key)
         return cls(
             client=client,
-            model=model,
+            deployment=deployment,
             storage=storage,
-            image_to_image_supported=image_to_image_supported,
-            width_constraints=image_width_constraints,
-            height_constraints=image_height_constraints,
+            spec=_get_spec(deployment.reference_deployment_id),
         )
+
+    async def configuration(self) -> Type[BaseModel]:
+        return self.spec.configuration_cls
 
     async def compute_discarded_messages(
         self, params: ModelParameters, messages: List[Message]
@@ -155,23 +206,26 @@ class StabilityV2Adapter(ChatCompletionAdapter):
         messages: List[Message],
     ) -> None:
 
+        configuration = params.parse_configuration(await self.configuration())
+        configuration_dict = (
+            {} if configuration is None else configuration.dict()
+        )
+
         message = validate_last_message(messages)
         text_prompt, image_resources = parse_message(
             message, SUPPORTED_IMAGE_TYPES
         )
 
-        if not self.image_to_image_supported and image_resources:
+        if not self.spec.image_to_image_supported and image_resources:
             raise UserError("Image-to-Image is not supported")
         if len(image_resources) > 1:
             raise UserError("Only one input image is supported")
 
-        if self.image_to_image_supported and image_resources:
+        if self.spec.image_to_image_supported and image_resources:
             image_resource = await _download_resource(
                 image_resources[0], self.storage
             )
-            _validate_image_size(
-                image_resource, self.width_constraints, self.height_constraints
-            )
+            self.spec.validate_image(image_resource)
         else:
             image_resource = None
 
@@ -179,7 +233,7 @@ class StabilityV2Adapter(ChatCompletionAdapter):
             raise UserError("Text prompt is required")
 
         response, _ = await self.client.ainvoke_non_streaming(
-            self.model,
+            self.deployment.upstream_deployment_id,
             remove_nones(
                 {
                     "prompt": text_prompt,
@@ -194,6 +248,8 @@ class StabilityV2Adapter(ChatCompletionAdapter):
                     # where 0 means that output will be identical to input image and 1 means that model will ignore input image
                     # Since there is no recommended default value, we use 0.5 as a middle ground
                     "strength": 0.5 if image_resource else None,
+                    "seed": params.seed,
+                    **configuration_dict,
                 }
             ),
         )
