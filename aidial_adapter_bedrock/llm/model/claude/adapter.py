6bdf9a97628d1d65df15a6b79b12e90fcc737697
Anton Dubovik <anton_dubovik@epam.com>
2026-01-14 11:57:14 +0000

    fix: improve tokenizer

--- aidial_adapter_bedrock/llm/model/claude/adapter.py
+++ aidial_adapter_bedrock/llm/model/claude/adapter.py
@@ -1,7 +1,12 @@
+import dataclasses
+from typing import Literal, assert_never
+
 from aidial_adapter_anthropic.adapter import ChatCompletionAdapter
+from aidial_adapter_anthropic.adapter.claude import CrudeClaudeTokenizer
 from aidial_adapter_anthropic.adapter.claude import (
     create_adapter as create_anthropic_adapter,
 )
+from typing_extensions import override
 
 from aidial_adapter_bedrock.bedrock import create_anthropic_client
 from aidial_adapter_bedrock.deployments import ChatCompletionDeployment as D
@@ -36,6 +41,45 @@ def _supports_documents(deployment: ClaudeDeployment) -> bool:
     }
 
 
+@dataclasses.dataclass
+class _Tokenizer(CrudeClaudeTokenizer):
+    deployment: ClaudeDeployment
+
+    @override
+    def _tokenize_tool_system_message(
+        self, tool_choice: Literal["none", "auto", "any", "tool"]
+    ) -> int:
+        # See for the reference:
+        # https://platform.claude.com/docs/en/agents-and-tools/tool-use/overview#pricing
+        def _tokens() -> tuple[int, int]:
+            match self.deployment:
+                case D.ANTHROPIC_CLAUDE_V3_5_SONNET:
+                    return (294, 261)
+                case (
+                    D.ANTHROPIC_CLAUDE_V3_5_SONNET_V2
+                    | D.ANTHROPIC_CLAUDE_V3_7_SONNET
+                    | D.ANTHROPIC_CLAUDE_V4_SONNET
+                    | D.ANTHROPIC_CLAUDE_V4_OPUS
+                    | D.ANTHROPIC_CLAUDE_V4_1_OPUS
+                    | D.ANTHROPIC_CLAUDE_V4_5_HAIKU
+                    | D.ANTHROPIC_CLAUDE_V4_5_SONNET
+                ):
+                    return (346, 313)
+                case D.ANTHROPIC_CLAUDE_V3_OPUS:
+                    return (530, 281)
+                case D.ANTHROPIC_CLAUDE_V3_SONNET:
+                    return (159, 235)
+                case (
+                    D.ANTHROPIC_CLAUDE_V3_HAIKU | D.ANTHROPIC_CLAUDE_V3_5_HAIKU
+                ):
+                    return (264, 340)
+                case _:
+                    assert_never(self.deployment)
+
+        (tokens1, tokens2) = _tokens()
+        return tokens1 if tool_choice in ("auto", "none") else tokens2
+
+
 async def create_adapter(
     deployment: AdapterDeployment[ClaudeDeployment],
     api_key: str,
@@ -48,6 +92,7 @@ async def create_adapter(
         deployment=deployment.upstream_deployment_id,
         storage=create_file_storage(api_key),
         client=client,
+        tokenizer=_Tokenizer(deployment.reference_deployment_id),
         default_max_tokens=CLAUDE_DEFAULT_MAX_TOKENS,
         supports_thinking=_supports_thinking(ref),
         supports_documents=_supports_documents(ref),
