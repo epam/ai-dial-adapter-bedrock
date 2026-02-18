382ae8dec9d91baaf1fa9f10e6786fa9df4e3aa3
Timur Golybovich <timur_golybovich@epam.com>
2026-02-18 19:06:30 +0100

    fixed mr remarks iteration 4

--- tests/integration_tests/test_chat_completion.py
+++ tests/integration_tests/test_chat_completion.py
@@ -214,7 +214,6 @@ def supports_json_object_response_format(deployment: D) -> bool:
     return deployment in [
         D.ANTHROPIC_CLAUDE_V3_5_SONNET,
         D.ANTHROPIC_CLAUDE_V3_5_SONNET_V2,
-        D.ANTHROPIC_CLAUDE_V3_5_HAIKU,
         D.ANTHROPIC_CLAUDE_V3_7_SONNET,
         D.ANTHROPIC_CLAUDE_V4_OPUS,
         D.ANTHROPIC_CLAUDE_V4_1_OPUS,
